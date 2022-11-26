4afc0c20719eba51849cd6447dc3784fc7acfcc1
kruk97 <bartekandrzejtkaczyk@gmail.com>
2022-11-26 17:12:40 +0100

    task1

---
+++ odejmowanie.py
@@ -0,0 +1,10 @@
+# Uzupełnij funkcję `subtract` tak, aby przyjmowała jeden argument
+# pozycyjny i zwracała funkcję. Zwrócona funkcja powinna przyjmować
+# jeden argument pozycyjny i zwracać wynik odejmowania otrzymanego argumentu i
+# argumentu przekazanego wcześniej do funkcji subtract
+#
+# Przykład:
+# substact(1)(2) == -1
+
+def subtract(numver):
+    pass
